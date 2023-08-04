d727264b3240b81e5a1cf6f256a03844d4e77076
cdeeef <5286cleaff@gmail.com>
2023-08-04 15:03:27 +0800

    init

---
+++ svn-unreal-create-repo-gui/Form1.cs
@@ -0,0 +1,15 @@
+namespace svn_unreal_create_repo_gui
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
